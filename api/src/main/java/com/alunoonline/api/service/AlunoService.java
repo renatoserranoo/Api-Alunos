977fe3917e3a4071b366426c173c11f3671bd465
renatoserranoo <renatoserranoribeiro@gmail.com>
2023-04-11 19:20:31 -0300

    Initial commit

---
+++ api/src/main/java/com/alunoonline/api/service/AlunoService.java
@@ -0,0 +1,16 @@
+package com.alunoonline.api.service;
+
+import com.alunoonline.api.model.Aluno;
+import com.alunoonline.api.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AlunoService {
+    @Autowired
+    AlunoRepository repository;
+
+    public Aluno create(Aluno aluno) {
+        return repository.save(aluno);
+    }
+}
